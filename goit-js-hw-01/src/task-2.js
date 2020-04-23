7701a71194923b565f8218fdc8ec39dc7cb2d4dd
SkuzIgor <2401170@gmail.com>
2020-04-23 15:58:31 +0300

    task-02 done

---
+++ goit-js-hw-01/src/task-2.js
@@ -0,0 +1,8 @@
+"use strict";
+const total = 100;
+const ordered = 30;
+if (ordered > total) {
+  console.log('На складе недостаточно товаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер.');
+}
